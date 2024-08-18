b883dcf18cf142c12aad048c485ebe9a35061391
duro06 <andi.meka.025@gmail.com>
2024-08-18 16:01:46 +0700

    anu laporan

--- src/pages/report/MainPage.vue
+++ src/pages/report/MainPage.vue
@@ -1,7 +1,19 @@
 <template>
   <div class="q-pa-sm">
-    <div class="row full-width q-col-gutter-sm q-mb-xs items-center">
-      <div class="col-auto">pilihan</div>
+    <div class="row full-width q-col-gutter-sm q-mb-xs items-center justify-between">
+      <div class="col-auto" style="min-width: 100px; max-width: 50%;">
+        <q-select v-model="selctModel"
+        label="pilih tanggal"
+        dense
+        outlined
+        clearable
+        use-input
+        input-debounce="400"
+        :options="options"
+        @update:model-value="selected"
+        @filter="filterFn"
+        ></q-select>
+      </div>
       <div class="col-auto">
         <q-btn
         dense
@@ -38,7 +50,7 @@
       >
       <template v-for="ta in tabs" :key="ta">
         <q-tab-panel :name="ta.name">
-          <laporanPage/>
+          <laporanPage :laporan="ta?.laporan"/>
         </q-tab-panel>
       </template>
     </q-tab-panels>
@@ -47,13 +59,13 @@
 </template>
 <script setup>
 import { api } from 'src/boot/axios'
-import { notifSuccess } from 'src/modules/utils'
-import { defineAsyncComponent, ref } from 'vue'
+import { filterDuplicateArrays, notifSuccess } from 'src/modules/utils'
+import { defineAsyncComponent, onMounted, ref } from 'vue'
 const laporanPage = defineAsyncComponent(() => import('src/pages/report/comp/LaporanPage.vue'))
 const tab = ref('semua')
-const tabs = [
-  { label: 'Semua', name: 'semua' }
-]
+const tabs = ref([
+  { label: 'Semua', name: 'semua', laporan: {} }
+])
 // kirim notif
 const loadingKirimNotif = ref(false)
 async function kirimNotif () {
@@ -67,4 +79,110 @@ async function kirimNotif () {
     })
     .catch(() => { loadingKirimNotif.value = false })
 }
+// ambil laporan yang sudah ada
+const loadingAmbil = ref(false)
+const reports = ref([])
+function getSavedReports (val) {
+  const param = {
+    params: {
+      q: val
+    }
+  }
+  loadingAmbil.value = true
+  return new Promise(resolve => {
+    api.get('v1/report/get-saved-report', param)
+      .then(resp => {
+        loadingAmbil.value = false
+        console.log('resports', resp?.data)
+        makeReport(resp?.data)
+        resolve(resp?.data)
+      })
+      .catch(() => { loadingAmbil.value = false })
+  })
+}
+// make report
+function makeReport (val) {
+  const report = val
+  if (report?.length) {
+    const tgl = report.map(m => m.tgl)
+    options.value = filterDuplicateArrays(tgl)
+    const kodecabang = filterDuplicateArrays(report.map(m => m.kodecabang))
+    const laporan = {}
+    const keyLaporan = Object.keys(report[0]?.laporan)
+    if (keyLaporan?.length) {
+      laporan.masterBeban = []
+      keyLaporan.forEach(key => {
+        if (key === 'masterBeban') {
+          report[0]?.laporan?.masterBeban.forEach(la => {
+            const bebebe = {
+              id: la.id,
+              nama: la.nama,
+              total: 0
+            }
+            laporan.masterBeban.push(bebebe)
+          })
+        } else laporan[key] = 0
+      })
+    }
+    if (kodecabang.length) {
+      kodecabang.forEach(cab => {
+        const temp = report.find(fi => fi.kodecabang === cab)
+        if (temp) {
+          const mau = {
+            label: temp?.cabang?.namacabang,
+            name: temp?.kodecabang,
+            laporan: temp?.laporan
+          }
+          tabs.value.push(mau)
+          console.log('kodecabang', mau)
+          const keys = Object.keys(temp?.laporan)
+          if (keys.length) {
+            keys.forEach(key => {
+              if (key === 'masterBeban') {
+                temp?.laporan?.masterBeban?.forEach(beb => {
+                  const beban = laporan?.masterBeban.find(f => f.id === beb.id)
+                  if (beban) {
+                    beban.total += beb.total
+                  }
+                })
+              } else laporan[key] += temp?.laporan[key]
+            })
+          }
+        }
+      })
+      const anu = tabs.value.find(f => f.name === 'semua')
+      if (anu) anu.laporan = laporan
+    }
+  }
+  reports.value = val
+}
+// buat select
+const selctModel = ref('')
+const options = ref([])
+function selected (val) {
+  const ada = reports.value?.filter(f => f.tgl === val)
+  console.log('selected', val, ada)
+}
+function filterFn (val, update) {
+  const prevOpt = options.value
+  if (val === '') {
+    update(() => {
+      options.value = prevOpt
+    })
+  }
+  if (val?.length < 3) {
+    update(() => {
+      const needle = val.toLowerCase()
+      options.value = prevOpt.filter(v => v.toLowerCase().indexOf(needle) > -1)
+    })
+  } else {
+    update(() => {
+      getSavedReports(val)
+    })
+  }
+  console.log('selected', val)
+}
+onMounted(() => {
+  getSavedReports()
+})
 </script>
--- src/pages/report/comp/LaporanPage.vue
+++ src/pages/report/comp/LaporanPage.vue
@@ -1,3 +1,59 @@
 <template>
-  laporan
+  <div>
+    <div class="row text-weight-bold">
+      A. Penjualan
+    </div>
+    <div class="row items-center justify-between" style="border-bottom: 1px solid black;">
+      <div class="col-6">Penjualan</div>
+      <div class="col-6 text-right text-green">{{formatDouble( laporan?.penjualanP)}}</div>
+    </div>
+    <div class="row items-center justify-between" style="border-bottom: 1px solid black;">
+      <div class="col-6">Retur Penjualan</div>
+      <div class="col-6 text-right text-green">{{formatDouble( laporan?.returPenjualanP)}}</div>
+    </div>
+    <div class="row items-center justify-between" style="border-bottom: 1px solid black;">
+      <div class="col-6">Penjualan Bersih</div>
+      <div class="col-6 text-right text-green">{{formatDouble( laporan?.penjualanBersih)}}</div>
+    </div>
+    <div class="row text-weight-bold q-mt-sm">
+      B. Harga Pokok Penjualan (HPP)
+    </div>
+    <div class="row items-center justify-between" style="border-bottom: 1px solid black;">
+      <div class="col-6">HPP</div>
+      <div class="col-6 text-right text-negative">{{formatDouble( laporan?.hpp)}}</div>
+    </div>
+    <div class="row items-center justify-between" style="border-bottom: 1px solid black;">
+      <div class="col-6">Laba kotor</div>
+      <div class="col-6 text-right text-green">{{formatDouble( laporan?.penjualanBersih - laporan?.hpp)}}</div>
+    </div>
+    <div class="row text-weight-bold q-mt-sm">
+      C. Pengeluaran
+    </div>
+    <div v-if="laporan?.masterBeban?.length">
+      <div v-for="beban in laporan?.masterBeban" :key="beban" class="row items-center justify-between" style="border-bottom: 1px solid black;">
+        <div class="col-6">{{beban?.nama}}</div>
+        <div class="col-6 text-right text-negative">{{formatDouble( beban?.total)}}</div>
+      </div>
+
+    </div>
+    <div class="row items-center justify-between" style="border-bottom: 1px solid black;">
+      <div class="col-6">Total Pengeluaran</div>
+      <div class="col-6 text-right text-negative">{{formatDouble( laporan?.totBeban)}}</div>
+    </div>
+    <div v-if="laporan?.labaRugi>0" class="row items-center justify-between" style="border-bottom: 1px solid black;">
+      <div class="col-6">Laba Bersih</div>
+      <div class="col-6 text-right text-greed">{{formatDouble( laporan?.labaRugi)}}</div>
+    </div>
+    <div v-if="laporan?.labaRugi<=0" class="row items-center justify-between" style="border-bottom: 1px solid black;">
+      <div class="col-6">Rugi</div>
+      <div class="col-6 text-right text-negative">{{formatDouble( laporan?.labaRugi)}}</div>
+    </div>
+  </div>
 </template>
+<script setup>
+import { formatDouble } from 'src/modules/formatter'
+
+defineProps({
+  laporan: { type: Object, default: () => {} }
+})
+</script>
